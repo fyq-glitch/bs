7c0170fc62c10616c936d63e19f5a3ef8aebbd97
yueqingfan <yueqingfan@outlook.com>
2025-05-09 17:40:49 +0800

    DA

--- model/train.py
+++ model/train.py
@@ -2,11 +2,12 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     #resume=True
     #r"C:\Users\fyq\Desktop\bs\model\runs\detect\train8\weights\last.pt"
-    model = YOLO(r"C:\Users\fyq\Desktop\bs\model\runs\detect\n21\weights\last.pt")
-    results = model.train(data=r"C:\Users\fyq\Desktop\dataset\resized\data21.yaml",
+    model = YOLO(r"yolo11n.pt")
+    results = model.train(data=r"C:\Users\fyq\Desktop\dataset\resized\data31.yaml",
+                          cfg=r"C:\Users\fyq\Desktop\dataset\resized\cfg.yaml",
                           epochs=100,
                           imgsz=960,
                           batch=16,
                           device=0,
-                          name="n21",
+                          name="n31p",
                           resume=True)
--- model/val.py
+++ model/val.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 if __name__ == '__main__':
-    model = YOLO(r"C:\Users\fyq\Desktop\bs\model\runs\detect\x12\weights\best.pt")
-    val_results = model.val(data=r"C:\Users\fyq\Desktop\dataset\resized\data2.yaml",
+    model = YOLO(r"C:\Users\fyq\Desktop\bs\model\runs\detect\n12p\weights\best.pt")
+    val_results = model.val(data=r"C:\Users\fyq\Desktop\dataset\resized\data3.yaml",
                             batch=16,
                             imgsz=960,
                             plots=True,
                             iou=0.5,
                             device=0,
-                            name="val_12x",
+                            name="val_13p",
                             save=True,
                             )
