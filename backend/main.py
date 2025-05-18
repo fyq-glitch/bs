8c2ecea6337c31cedf7274147a7b5767f3f04ab2
yueqingfan <yueqingfan@outlook.com>
2025-05-19 02:01:02 +0800

    beautify ui

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7abefb744dc71022bd50ff6e8dd16c5c7af3095561f9420b165d43108c622290
-size 4555
+oid sha256:a230e2a83f082aefa6baf25eb352487a19e7241a5a795d5e7e5fa2013f36202d
+size 4583
--- model/train.py
+++ model/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd3a64571fca8c7b6521e87511ed2c8cc6ac479cad7b22627f0867316059e220
-size 556
+oid sha256:0d85392e82eaf264d3863e4dca935021dfdd88c4955364c79082f3ec91625662
+size 648
